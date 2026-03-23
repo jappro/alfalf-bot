59de27b180e9eccf302e9324980520cd435153d2
Japheth | RpT <camzyxworld@gmail.com>
2026-03-23 12:42:37 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "alfalf-bot",
+  "version": "1.0.0",
+  "description": "Alfalf AI - Campaign Architect for Web3",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "node-telegram-bot-api": "^0.64.0",
+    "groq-sdk": "^0.3.0",
+    "dotenv": "^16.0.0"
+  }
+}
